3bf3db551f9e3406773ed4c1bcbe8d4ce13f0ad2
H. Emre Aral <78971902+emrearal@users.noreply.github.com>
2022-02-09 11:52:52 +0300

    Update dovizver.php

--- dovizver.php
+++ dovizver.php
@@ -1,4 +1,5 @@
 <?php
+// emre@aral.web.tr
 function dovizkuruver($tarih) {
     $tarih= (strtotime($tarih)>time()) ? (date('Y-m-d')) : $tarih; // sorgu ileri tarihli ise bakılacak tarih bugünün tarihidir.
     $kaydedilecektarih=$bakilantarih = date('Y-m-d', strtotime( $tarih . " -1 days")); // hep bir gün öncesine bakacağız
